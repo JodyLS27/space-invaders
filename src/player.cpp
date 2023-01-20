39dd6570389d651d9cc3b6460c4ba18b7de96587
Jody <jodystocks01@gmail.com>
2023-01-20 07:15:55 +0200

    Added some more Functionality to the Player Header and source file.

--- src/player.cpp
+++ src/player.cpp
@@ -2,10 +2,33 @@
 
 namespace player
 {
+	Player::Player()
+		: m_score(0), m_jump_height(20), m_can_jump(true)
+	{}
+
 	void Player::set_position(sf::Vector2f position)
 	{
 		m_position = position;
 	}
 
+	void Player::set_direction(sf::Vector2i direction)
+	{
+			
+	}
 
+	void Player::set_shape(sf::Vector2i shape_size, sf::Color color = sf::Color::Red)
+	{
+
+	}
+
+
+	sf::Vector2f Player::get_position()
+	{
+		return m_position;
+	}
+
+	sf::RectangleShape Player::get_shape()
+	{
+		return m_shape;
+	}
 }
--- src/player.hpp
+++ src/player.hpp
@@ -6,18 +6,26 @@ namespace player
 	class Player
 	{
 	private:
+		Player();
+
 		sf::Vector2f m_position{};
+		sf::Vector2i m_direction{};
+		sf::RectangleShape m_shape{};
 
-		int m_jump_height{};
 		int m_score{};
-
+		int m_jump_height{};
 		bool m_can_jump{};
+
+		void update_position(sf::Vector2i direction);
 
 	public:
 		void set_position(sf::Vector2f position);
-
+		void set_direction(sf::Keyboard::Key);
+		void set_shape(sf::Vector2i shape_size, sf::Color color = sf::Color::Red);
+		
 
 		sf::Vector2f get_position();
+		sf::RectangleShape get_shape();
 
 	};
 

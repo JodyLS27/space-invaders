f5f47f644e3d06a53a15d212c991a94c7e4a7f0e
Jody Stocks <jodystocks01@gmail.com>
2023-06-27 21:47:56 +0200

    Updating files - Added debugging logger function to the logger.hpp file - Updated the CMakeLists with the new logger files

--- src/logger.hpp
+++ src/logger.hpp
@@ -6,3 +6,27 @@
 #if defined (WIN32)
 #include <Windows.h>
 #endif
+
+namespace logger
+{
+struct Logger
+{
+	/////////////////////////////////////////////////////////
+	/// This function will print your log message to the terminal.
+	/// 
+	/// @param msg: String parameter containing an output message.
+	/// 
+	/// @return Type: void
+	/////////////////////////////////////////////////////////
+	void print_log(std::string msg);
+
+	/////////////////////////////////////////////////////////
+	/// This function will print your log message to the debug window based on your OS.
+	/// 
+	/// @param msg: String parameter containing an output message.
+	/// 
+	/// @return Type: void
+	/////////////////////////////////////////////////////////
+	void debug_log(std::string msg);
+};
+}

e0799e68fc9be4b3351d44c5b54d30961dfa43ac
Jody Stocks <jodystocks01@gmail.com>
2023-08-29 22:11:19 +0200

    Creating a more modular Cmake structure - Added Cmake Folder with FetchSFML and TargetSources .cmake files. - Updating the CMakeLists.txt file

---
+++ cmake/FetchSFML.cmake
@@ -0,0 +1,12 @@
+# Including the fetch content for SFML
+include(FetchContent)
+set(BUILD_SHARED_LIBS OFF)
+message(STATUS "Fetching SFML....")
+
+FetchContent_Declare(
+    sfml
+    GIT_REPOSITORY "https://github.com/SFML/SFML.git"
+    GIT_TAG 2f11710abc5aa478503a7ff3f9e654bd2078ebab
+    SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/ext/sfml"
+)
+FetchContent_MakeAvailable(sfml)

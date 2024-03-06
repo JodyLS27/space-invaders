9b65015324b5273e584b897661506d9145869ae3
Jody L. Stocks <jodystocks01@gmail.com>
2024-03-06 16:27:40 +0200

    Function Docs - Added Function docs for game_manager::init() and game_manager::update()

--- include/core/game_manager.hpp
+++ include/core/game_manager.hpp
@@ -3,8 +3,20 @@
 
 namespace game_manager
 {
-	// Attributes
-
+	/**
+	* Game initialization
+	*
+	* This function will load up the Default main manu and the SFML main window in the future.
+	*
+	**/
 	void init();
+
+	/**
+	* Primary game loop
+	*
+	* This Function will enter the primary game loop and update all menue's, Game Modes and characters.
+	*
+	* @param menu_manager: Object to manage the menus in the game
+	**/
 	void update(menu::MenuManager& menu_manager);
 }
--- src/core/game_manager.cpp
+++ src/core/game_manager.cpp
@@ -2,21 +2,24 @@
 
 void game_manager::init()
 {
-	// TODO: Test the main_menu structure
-	//		- Load the Init() on the MenuManager class (Game starts here)
 	// TODO: Setup the player controller ptr	//	
 	// Call Update function to get the main game loop moving.
-
+	
+	// Load up the main menu
 	menu::MenuManager menu_manager;
 	menu_manager.init();
 
+	// TODO: Rename this function to be more meaningful
+	// Run the main game loop
 	update(menu_manager);
 }
 
 void game_manager::update(menu::MenuManager& menu_manager)
 {
+	// Test Value for now. Will switch this out to the Manager Later.
 	int16_t option{};
 
+	// Main game loop start
 	while (true)
 	{
 		// Call current state Update and draw.

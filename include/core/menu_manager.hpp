25383ea077b27a1608d9b13b1b09f3a334904d51
Jody L. Stocks <jodystocks01@gmail.com>
2024-03-06 21:26:43 +0200

    Adding Function docs - Adding Function docs for Clarity going forward - Added Docs to menu_manager.hpp.

--- include/core/menu_manager.hpp
+++ include/core/menu_manager.hpp
@@ -15,15 +15,61 @@ namespace menu
 		MenuManager();
 		~MenuManager();
 
+		/**
+		* Set the current menu option
+		*
+		* @note: This holds a number for now just for Testing and will be updated later on
+		* to another system
+		*
+		* @param current_option: Stores an int16_t of the current selction
+		**/
 		void set_current_option(int16_t current_option);
+
+		/**
+		* Set the Next menu option
+		*
+		* @note: This holds a number for now just for Testing and will be updated later on
+		* to another system
+		*
+		* @param next_option: Stores an int16_t of the next selction
+		**/
 		void set_next_option(int16_t next_option);
 
+		/**
+		* Get the current menu option
+		*
+		* @note: This holds a number for now just for Testing and will be updated later on
+		* to another system
+		**/
 		const int16_t get_current_option();
+
+		/**
+		* Get the next menu option
+		*
+		* @note: This holds a number for now just for Testing and will be updated later on
+		* to another system
+		**/
 		const int16_t get_next_option();
 
-		// Primary functions of the Menu manager for the current state
-		void init(); // TODO: Call the current state set_context() Function to pass in this context ref.
+		/**
+		* Initialize the menu controller.
+		*
+		* The function is going to set the default menu option on game first load
+		**/
+		void init(); // TODO: Call the current state set_context() Function to pass in this context ref
+
+		/**
+		* Update the menus each frame
+		*
+		* This function is designed to run the current menu object's update function
+		**/
 		void update();
+
+		/**
+		* Draw the menu to the screen
+		*
+		* This function is used to render the current menu options to the screen
+		**/
 		void draw();
 
 		void switch_menu(std::unique_ptr<IMenu> new_menu);
@@ -32,7 +78,12 @@ namespace menu
 		std::unique_ptr<IMenu> next_menu;
 
 	protected:
-		// Pointer for the current menu to load
+		/**
+		* The current menu in use
+		*
+		* This is a unique pointer that holds one menu object at a time
+		*
+		**/
 		std::unique_ptr<IMenu> current_menu;
 
 	private:
--- include/menu/i_menu.hpp
+++ include/menu/i_menu.hpp
@@ -4,21 +4,14 @@
 
 namespace menu
 {
-	// Forward declare
-	class MenuManager;
-
 	class IMenu
 	{
-	public: 
+	public:
 		virtual ~IMenu() = default;
 
 		virtual void init() = 0;
 		virtual void update() = 0;
 		virtual void draw() = 0;
 		virtual void set_menu_controller() = 0;
-
-	protected:
-		// Reference should be set by set_context().
-		MenuManager& m_menu_manager;
 	};
 }

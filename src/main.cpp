88a42011126c5c1118da5c16dbad19240ac706d7
JodyLStocks <jodystocks01@gmail.com>
2025-10-07 19:31:46 +0200

    Updating main - Cleanup of imports and removed the include of `main.h` and it's not in the project - Clight cleanup of code padding

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include "core/game_manager.hpp"
-#include "main.h"
 
 int main()
 {
-	game_manager::init();
-	game_manager::start();
+    game_manager::init();
+    game_manager::start();
 
-	game_manager::exit();
+    game_manager::exit();
 }

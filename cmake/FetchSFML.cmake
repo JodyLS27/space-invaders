6deae2d51efd7eb1de3de2aa65a7898607ea71d9
JodyLStocks <jodystocks01@gmail.com>
2025-10-07 19:19:16 +0200

    Updating CMake files - Updated the version of the Project to be `0.1.0` as its a better starting point with semantic versioning::`/CMakeLists.txt` - Updated `/cmake/FetchSFML.cmake` to allow SFML to be used locally after being cloned down and not Reference GitHub during each built.

--- cmake/FetchSFML.cmake
+++ cmake/FetchSFML.cmake
@@ -1,19 +1,28 @@
 # Including the fetch content for SFML
 include(FetchContent)
 set(BUILD_SHARED_LIBS OFF)
-message(STATUS "Fetching SFML....")
 
-FetchContent_Declare(
-    sfml
-    GIT_REPOSITORY "https://github.com/SFML/SFML.git"
-    GIT_TAG 69ea0cd863aed1d4092b970b676924a716ff718b # v2.6.1
-    SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/libs/sfml"
-)
+# Setup local directory for checking
+set(SFML_LOCAL "${CMAKE_CURRENT_SOURCE_DIR}/libs/sfml")
+
+# Check if Local Copy exists
+if (EXISTS ${SFML_LOCAL}/CMakeLists.txt)
+    message(STATUS "Using local copy of SFML at :${SFML_LOCAL}")
+
+    # Set local fetchcontent
+    FetchContent_Declare(
+            sfml
+            SOURCE_DIR ${SFML_LOCAL}
+    )
+else ()
+    message(STATUS "Fetching SFML from GitHub....")
+
+    FetchContent_Declare(
+            sfml
+            GIT_REPOSITORY "https://github.com/SFML/SFML.git"
+            GIT_TAG "3.0.2"
+            SOURCE_DIR "${SFML_LOCAL}"
+    )
+endif ()
+
 FetchContent_MakeAvailable(sfml)
-
-
-if(sfml_FOUND)
-    message(STATUS "--- SFML Downloaded ---")
-else()
-    message(STATUS "!--- SFML Not found! ---!")
-endif()
